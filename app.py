ad45633e654e200a9ac5b9b8cc2660263372da66
Jaiminml <61429107+Jaiminml@users.noreply.github.com>
2022-09-09 00:55:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template
+from flask import request
+import requests
+import ast
+
+app = Flask(__name__)
+
+def get_t5_response(text):
+    r = requests.post(
+        url="https://hf.space/embed/jaimin/Paraphrase/+/api/predict",
+        json={"data": [text]},
+    )
+    response = r.json()
+    return response["data"][0],response["data"][1]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        form = request.form
+        # print(form,'form')
+        result = []
+        result1 = []
+        #bert_abstract = form['paragraph']
+        question = form['question']
+        print(question,'question')
+        #result.append(form['question'])
+        text1,text2 = get_t5_response(question)
+        text1 = ast.literal_eval(text1)
+        text2 = ast.literal_eval(text2)
+        result.append(text1)
+        result.append(form['question'])
+        result1.append(text2)
+        result1.append(form['question'])
+        return render_template("index.html", result=result,result1=result1)
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0')
